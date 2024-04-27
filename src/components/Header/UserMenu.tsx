9161c681d77a3d4439c13a1c0fe512c4643d789d
EddyerDevv <urbinaramalazo1986@gmail.com>
2024-04-27 17:21:25 -0600

    add: an accent option was added to the buttons props of the user menu

--- src/components/Header/UserMenu.tsx
+++ src/components/Header/UserMenu.tsx
@@ -17,6 +17,7 @@ interface ButtonProps {
   href?: string;
   icon: React.ReactNode;
   spacer?: boolean;
+  accent?: boolean;
 }
 
 const buttonIconSize = "w-[1.15rem] h-[1.15rem]";
@@ -37,6 +38,7 @@ const buttonArray: ButtonProps[] = [
     label: "Sign Out",
     icon: <LogOutIcon className={buttonIconSize} />,
     spacer: false,
+    accent: true,
   },
 ];
 
@@ -140,7 +142,12 @@ export default function UserMenu({
             {button.href ? (
               <Link
                 href={button.href}
-                className="w-full flex justify-between items-center px-2 h-[2.1rem] gap-2 rounded-md cursor-pointer border-[1px] border-neutral-700 bg-neutral-900 bg-opacity-30 text-neutral-200 hover:text-emerald-300 hover:bg-neutral-600 hover:bg-opacity-20 hover:border-neutral-500  transition-colors ease-in-out duration-[.25s]"
+                className={`
+                  ${
+                    button.accent
+                      ? "bg-emerald-600 bg-opacity-50 border-emerald-500 text-white hover:bg-opacity-60"
+                      : "border-neutral-700 bg-neutral-800 bg-opacity-10 text-neutral-300 hover:text-white hover:bg-neutral-600 hover:bg-opacity-20 hover:border-neutral-600 hover:border-opacity-95"
+                  } w-full flex justify-between items-center px-2 h-[2.1rem] gap-2 rounded-md cursor-pointer border-[1px] text-neutral-200  transition-colors ease-in-out duration-[.25s]`}
                 onClick={() => setActiveMenuUser(false)}
               >
                 <span className="mt-[0.025rem] leading-[0] font-normal font-geist-sans text-[.885rem]">
@@ -150,7 +157,12 @@ export default function UserMenu({
               </Link>
             ) : (
               <button
-                className="w-full flex justify-between items-center px-2 h-[2.1rem] gap-2 rounded-md cursor-pointer border-[1px] border-neutral-700 bg-neutral-900 bg-opacity-30 text-neutral-200 hover:text-emerald-300 hover:bg-neutral-600 hover:bg-opacity-20 hover:border-neutral-500  transition-colors ease-in-out duration-[.25s]"
+                className={`
+                   ${
+                     button.accent
+                       ? "bg-emerald-600 bg-opacity-50 border-emerald-500 text-white hover:bg-opacity-60"
+                       : "border-neutral-700 bg-neutral-800 bg-opacity-10 text-neutral-300 hover:text-white hover:bg-neutral-600 hover:bg-opacity-20 hover:border-neutral-600 hover:border-opacity-95"
+                   } w-full flex justify-between items-center px-2 h-[2.1rem] gap-2 rounded-md cursor-pointer border-[1px] text-neutral-200  transition-colors ease-in-out duration-[.25s]`}
                 onClick={() => handleMenuSignOut()}
               >
                 <span className="mt-[0.025rem] leading-[0] font-normal font-geist-sans text-[.885rem]">
