f3c48fb0965cfc2ca116ed047f96b9a5f6360430
EddyerDevv <urbinaramalazo1986@gmail.com>
2024-04-26 22:12:15 -0600

    add: the sig en, the icon and as well as the user menu were added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,39 +1,102 @@
 "use client";
-import { memo } from "react";
+import { memo, useRef, useState } from "react";
 import { usePathname } from "next/navigation";
+import { useAuthState } from "@/providers/authState.provider";
+import { useSession } from "next-auth/react";
+import { GoogleIcon } from "./Icons";
+import Loader from "./Loader";
+import Image from "next/image";
 import Link from "next/link";
+import UserMenu from "./Header/UserMenu";
 
 const navLinks = [
-  { href: "/", label: "Dashboard" },
-  { href: "/latest-urls", label: "Latest Urls" },
+  { href: "/", label: "Latest Urls" },
+  { href: "/dashboard", label: "Dashboard" },
 ];
 
 function Header() {
+  const { state, handleSignIn } = useAuthState();
+  const { data: session, status } = useSession();
+  const [activeMenuUser, setActiveMenuUser] = useState(false);
+  const menuUserButtonRef = useRef<HTMLButtonElement>(null);
   const pathname = usePathname();
 
+  const handleActiveMenuUser = () => {
+    const appMenuUser = document.querySelector("#app_header_user_menu");
+
+    if (!(appMenuUser instanceof HTMLElement)) {
+      setActiveMenuUser(true);
+    } else {
+      appMenuUser.style.opacity = "0";
+
+      appMenuUser.addEventListener("transitionend", () => {
+        setActiveMenuUser(false);
+      });
+    }
+  };
+
   return (
-    <header className="bg-[var(--global-background-color)] h-16 border-b-[1px] border-neutral-700 flex justify-between items-center px-8 md:px-36">
+    <header className="bg-[var(--global-background-color)] bg-opacity-40 h-16 border-b-[1px] border-neutral-700 flex justify-between items-center px-8 md:px-[12%] transition-[padding] ease-in-out duration-[.2s] fixed top w-full backdrop-blur-md">
       <section className="flex flex-row justify-start items-center gap-6">
-        <div className="flex flex-row justify-center items-center ">
-          <h1 className="text-[1.3rem] font-semibold font-rubik text-stone-100">
+        <Link href="/" className="flex flex-row justify-center items-center ">
+          <h1 className="text-[1.3rem] font-semibold font-rubik text-gray-200">
             Link<span className="text-emerald-300">Snip</span>
           </h1>
-        </div>
+        </Link>
         <nav className="flex flex-row justify-center items-center gap-3 mt-[0.15rem]">
           {navLinks.map(({ href, label }) => (
             <Link
               key={href}
               href={href}
-              className={`text-stone-100 font-geist-sans font-semibold text-[.915rem] hover:text-emerald-300 transition-colors duration-[.25s] ease-in-out ${
-                pathname === href ? "text-emerald-300" : ""
-              }`}
+              className={`${
+                pathname === href ? "text-emerald-300" : "text-gray-200"
+              }
+               font-geist-sans font-semibold text-[.915rem] hover:text-emerald-300 transition-colors duration-[.25s] ease-in-out `}
             >
               {label}
             </Link>
           ))}
         </nav>
       </section>
-      <section></section>
+      <section className="flex flex-row justify-start items-center gap-4">
+        {state.loading ? (
+          <Loader size={"1.25rem"} />
+        ) : status === "authenticated" && state.loggedIn ? (
+          <article className="flex flex-col justify-center items-center relative">
+            <button
+              onClick={handleActiveMenuUser}
+              ref={menuUserButtonRef}
+              className="flex flex-row justify-center items-center rounded-full"
+            >
+              <Image
+                src={session.user!.image!}
+                alt="User Avatar"
+                width={50}
+                height={50}
+                className="rounded-full w-[2.2rem] h-[2.2rem]"
+                priority
+              />
+            </button>
+            {activeMenuUser && (
+              <UserMenu
+                activeMenuUser={activeMenuUser}
+                setActiveMenuUser={setActiveMenuUser}
+                menuUserButtonRef={menuUserButtonRef}
+              />
+            )}
+          </article>
+        ) : (
+          <button
+            onClick={handleSignIn}
+            className="bg-neutral-50 min-h-[2rem] px-[1.1rem] rounded-full flex flex-row justify-center items-center gap-[0.4rem]"
+          >
+            <GoogleIcon className="w-5 h-[1.25rem]" />
+            <span className="text-[1rem] mt-[.1rem] font-rubik font-semibold text-neutral-900">
+              Sign In
+            </span>
+          </button>
+        )}
+      </section>
     </header>
   );
 }
